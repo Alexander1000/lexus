9cf4d537cb52ab7d8e638513934d5c84a3645a51
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-05-02 11:31:56 +0300

    parse yaml file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <lexus.h>
+#include <io-buffer.h>
+#include <yaml-parser.h>
 
 int main(int argc, char** argv) {
     Lexus::Config config(argc, argv);
@@ -18,6 +20,13 @@ int main(int argc, char** argv) {
         std::cout << "Config file is required" << std::endl;
         return -1;
     }
+
+    IOBuffer::IOFileReader fileReader(*config.getConfigFile());
+    IOBuffer::CharStream charStream(&fileReader);
+    YamlParser::Stream yamlStream(&charStream);
+    YamlParser::Decoder decoder(&yamlStream);
+
+    YamlParser::Element* valuesObject = decoder.parse();
 
     return 0;
 }
